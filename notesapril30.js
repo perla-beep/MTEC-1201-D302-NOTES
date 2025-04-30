5e9e8338e5b4f8cc16d3932bcc1b012a72fc2705
perla-beep <perla.desantiago@mail.citytech.cuny.edu>
2025-04-30 16:31:09 -0400

    Update notesapril30.js

--- notesapril30.js
+++ notesapril30.js
@@ -1,1 +1,47 @@
 
+let drumLoop
+let bass
+let string
+
+let drumMute = false
+let bassMute = false
+
+function preload(){
+drumLoop = loadSound('audio/drumLoop.mp3')
+bass = loadSound('audio/bass.mp3')
+string = loadSound('audio/string.mp3')
+}
+
+function setup() {
+  createCanvas(800, 800);
+  strokeWeight(3)
+
+  drumLoop.loop()
+  bass.loop() // same beat overlapping
+}
+
+function draw() {
+  background(178);
+  
+  if(drumMute){
+    drumLoop.amp(0.0) // set volume to 0 no sound
+  }
+  else{
+    drumLoop.amp(1.0)//full volume
+    fill(220)
+    rect(0,height/2,width,height)
+  }
+}
+
+function mousePressed(){
+  string.play()
+}
+
+function keyPressed(){
+  if (key == 'a'){
+    drumMute = !drumMute
+  }
+  else if (key == 's'){
+    bassMute = !bassMute
+  }
+}
